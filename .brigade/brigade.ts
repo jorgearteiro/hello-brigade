6885843cd234cbe2b3f4b8f6cd52442ed4f458fc
Kent Rancourt <kent.rancourt@gmail.com>
2021-11-24 15:39:52 -0500

    Second commit
    Signed-off-by: Kent Rancourt <kent.rancourt@gmail.com>

--- .brigade/brigade.ts
+++ .brigade/brigade.ts
@@ -1,14 +1,28 @@
 import { events, Job } from "@brigadecore/brigadier"
+import { uniqueNamesGenerator, adjectives, animals } from "unique-names-generator"
         
 // Use events.on() to define how your script responds to different events. 
 // The example below depicts handling of "exec" events originating from
 // the Brigade CLI.
 
+function newName(): string {
+  return uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    length: 2,
+    separator: "-"
+  })
+}
+
 events.on("brigade.sh/cli", "exec", async event => {
-  let job = new Job("hello", "debian:latest", event)
-  job.primaryContainer.command = ["echo"]
-  job.primaryContainer.arguments = ["Hello, World!"]
-  await job.run()
+  let job1 = new Job("hello", "debian:latest", event)
+  job1.primaryContainer.command = ["echo"]
+  job1.primaryContainer.arguments = [`Hello, ${newName()}!`]
+  
+  let job2 = new Job("goodbye", "alpine:latest", event)
+  job2.primaryContainer.command = ["echo"]
+  job2.primaryContainer.arguments = [`Hello, ${event.project.secrets.name}!`]
+
+  await Job.sequence(job1, job2).run()
 })
 
 events.process()
